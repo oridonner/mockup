b7b0814040dc9f64e527b1e8076ffcb8d2e3aacf
Ori Donner <oridonner@gmail.com>
2023-04-19 17:08:04 +0000

    set gcp registry

--- docker-build.sh
+++ docker-build.sh
@@ -2,13 +2,13 @@
 
 echo "[INFO]: started building images..."
 
-docker build -t test314zdes/mock-defender:auth -f ./services/auth/Dockerfile .
-docker build -t test314zdes/mock-defender:faker -f ./services/faker/Dockerfile .
-docker build -t test314zdes/mock-defender:client -f ./clients/front/Dockerfile .
+docker build -t gcr.io/optimal-tide-293517/mock-defender:auth -f ./services/auth/Dockerfile .
+docker build -t gcr.io/optimal-tide-293517/mock-defender:faker -f ./services/faker/Dockerfile .
+docker build -t gcr.io/optimal-tide-293517/mock-defender:client -f ./clients/front/Dockerfile .
 
-docker push test314zdes/mock-defender:auth
-docker push test314zdes/mock-defender:faker
-docker push test314zdes/mock-defender:client
+#docker push gcr.io/optimal-tide-293517/mock-defender:auth
+#docker push gcr.io/optimal-tide-293517/mock-defender:faker
+#docker push gcr.io/optimal-tide-293517/mock-defender:client
 
 echo "
 [DONE]: succesfully built images.
