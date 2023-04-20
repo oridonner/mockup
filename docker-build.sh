a3b1c98e3aaca7be222c0497e0d887a62419fc43
Ori Donner <oridonner@gmail.com>
2023-04-20 14:59:07 +0000

    add artifact registry tag

--- docker-build.sh
+++ docker-build.sh
@@ -2,13 +2,21 @@
 
 echo "[INFO]: started building images..."
 
-docker build -t gcr.io/optimal-tide-293517/mock-defender:auth -f ./services/auth/Dockerfile .
-docker build -t gcr.io/optimal-tide-293517/mock-defender:faker -f ./services/faker/Dockerfile .
-docker build -t gcr.io/optimal-tide-293517/mock-defender:client -f ./clients/front/Dockerfile .
+# to docker registry
+# docker build -t gcr.io/optimal-tide-293517/mock-defender:auth -f ./services/auth/Dockerfile .
+# docker build -t gcr.io/optimal-tide-293517/mock-defender:faker -f ./services/faker/Dockerfile .
+# docker build -t gcr.io/optimal-tide-293517/mock-defender:client -f ./clients/front/Dockerfile .
+# docker push gcr.io/optimal-tide-293517/mock-defender:auth
+# docker push gcr.io/optimal-tide-293517/mock-defender:faker
+# docker push gcr.io/optimal-tide-293517/mock-defender:client
 
-#docker push gcr.io/optimal-tide-293517/mock-defender:auth
-#docker push gcr.io/optimal-tide-293517/mock-defender:faker
-#docker push gcr.io/optimal-tide-293517/mock-defender:client
+# to artifact registry
+docker build -t us-central1-docker.pkg.dev/optimal-tide-293517/wizdome:auth -f ./services/auth/Dockerfile .
+docker build -t us-central1-docker.pkg.dev/optimal-tide-293517/wizdome:faker -f ./services/faker/Dockerfile .
+docker build -t us-central1-docker.pkg.dev/optimal-tide-293517/wizdome:client -f ./clients/front/Dockerfile .
+docker push us-central1-docker.pkg.dev/optimal-tide-293517/wizdome:auth
+docker push us-central1-docker.pkg.dev/optimal-tide-293517/wizdome:faker
+docker push us-central1-docker.pkg.dev/optimal-tide-293517/wizdome:client
 
 echo "
 [DONE]: succesfully built images.
